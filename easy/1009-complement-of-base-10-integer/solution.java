017232a08a14a0d6c5e28f28ab911f68db3785de
RiShBhs-Code <rishbhh1617@gmail.com>
2026-08-05 20:25:04 +0530

    ✅ Complement of Base 10 Integer | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-05

---
+++ easy/1009-complement-of-base-10-integer/solution.java
@@ -0,0 +1,14 @@
+            if(rem == 0){
+                ans += Math.pow(2 , deg);
+            }
+            n /= 2 ;
+        }
+
+            deg++;
+        return ans;
+            int rem = n % 2 ;
+        while(n > 0) {
+
+        int ans = 0 ;
+        int deg = 0 ;
+
