e1582d1e23c0995d971a911e126a3849f70b1853
RiShBhs-Code <rishbhh1617@gmail.com>
2026-08-05 20:26:12 +0530

    ✅ Complement of Base 10 Integer | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-05

--- easy/1009-complement-of-base-10-integer/solution.java
+++ easy/1009-complement-of-base-10-integer/solution.java
@@ -1,14 +1,19 @@
-            if(rem == 0){
-                ans += Math.pow(2 , deg);
-            }
-            n /= 2 ;
-        }
+class Solution {
+    public int bitwiseComplement(int n) {
 
-            deg++;
-        return ans;
-            int rem = n % 2 ;
-        while(n > 0) {
+        int ans = 0 ;
+        int deg = 0 ;
 
-        int ans = 0 ;
-        int deg = 0 ;
+        while(n > 0) {
+            int rem = n % 2 ;
+            if(rem == 0){
+                ans += Math.pow(2 , deg);
+            }
+            n /= 2 ;
+            deg++;
+        }
 
+        return ans;
+        
+    }
+}
