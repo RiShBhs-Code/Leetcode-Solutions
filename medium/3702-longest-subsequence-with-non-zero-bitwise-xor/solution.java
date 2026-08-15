ed0e0e866a3dcd3da393909794d641981ecc0bc2
RiShBhs-Code <rishbhh1617@gmail.com>
2026-08-15 19:14:01 +0530

    ✅ Longest Subsequence With Non-Zero Bitwise XOR | Medium | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-15

---
+++ medium/3702-longest-subsequence-with-non-zero-bitwise-xor/solution.java
@@ -0,0 +1,13 @@
+
+        for(int x : nums) {
+            nonZero |= x > 0 ;
+        }
+
+            total ^= x;
+
+        if(nonZero) return 0;
+        boolean nonZero = false;
+        return total == 0 ? nums.length-1 : nums.length ;
+        
+    }
+}
