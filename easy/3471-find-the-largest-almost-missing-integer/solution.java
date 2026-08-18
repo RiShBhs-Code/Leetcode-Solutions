1343bec9039496069197b20297c9fccdb73915cb
RiShBhs-Code <rishbhh1617@gmail.com>
2026-08-18 20:55:00 +0530

    ✅ Find the Largest Almost Missing Integer | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-18

---
+++ easy/3471-find-the-largest-almost-missing-integer/solution.java
@@ -0,0 +1,11 @@
+            return max;
+        } else {
+            if(arr[nums[0]] == 1) {
+                return nums[0];
+            } else {
+                return nums[nums.length -1 ];
+            }
+        }
+        
+    }
+}
