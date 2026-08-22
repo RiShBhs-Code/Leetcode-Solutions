951912a71665fbafb2d496e8466bd550857c92ec
RiShBhs-Code <rishbhh1617@gmail.com>
2026-08-22 18:10:26 +0530

    ✅ Check Divisibility by Digit Sum and Product | Easy | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-08-22

---
+++ easy/3622-check-divisibility-by-digit-sum-and-product/solution.java
@@ -0,0 +1,13 @@
+    private static int value(int n){
+
+    }
+        
+        int val = value(n);
+
+    public boolean checkDivisibility(int n) {
+class Solution {
+
+        if(n % val == 0) 
+            return true;
+        
+        return false;
