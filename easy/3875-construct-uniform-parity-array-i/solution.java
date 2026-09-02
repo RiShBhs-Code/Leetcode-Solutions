7afe1de4921411a4a6cbb51c49f0712b8549e571
RiShBhs-Code <rishbhh1617@gmail.com>
2026-09-02 20:36:24 +0530

    ✅ Construct Uniform Parity Array I | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-09-02

--- easy/3875-construct-uniform-parity-array-i/solution.java
+++ easy/3875-construct-uniform-parity-array-i/solution.java
@@ -1,14 +1,17 @@
-        int odd = 0 ;
-        for(int num : nums1) {
-            if(num % 2 == 0)
-                even++;
-        }
-            else 
-                odd++;
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int even = 0 ;
+        int odd = 0 ;
+        for(int num : nums1) {
+            if(num % 2 == 0)
+                even++;
+            else 
+                odd++;
+        }
 
-        if ( even > odd && even > 0 && odd > 0)
-            return false;
-        int even = 0 ;
-    public boolean uniformArray(int[] nums1) {
-        
-        return true;
+        if ( even > odd && even > 0 && odd > 0)
+            return false;
+        
+        return true;
+    }
+}
