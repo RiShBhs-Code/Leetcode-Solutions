646ded0d728135aa10ccc65cae7a9e094600d48a
RiShBhs-Code <rishbhh1617@gmail.com>
2026-09-04 22:43:29 +0530

    ✅ Smallest Stable Index I | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-09-04

--- easy/3903-smallest-stable-index-i/solution.java
+++ easy/3903-smallest-stable-index-i/solution.java
@@ -1,14 +1,14 @@
+        int x = Integer.MAX_VALUE;
+        int ans = -1;
+
         for(int j = n - 1 ; j >= 0 ; j--){
             min = Math.min(min , nums[j]);
             int temp = arr[j] - min ;
-            if(temp <= x){
+            if(temp < k){
                 x = temp;
+                ans = j;
             }
         }
         return ans;   
     }
-
-        int x = Integer.MAX_VALUE;
-        int min = Integer.MAX_VALUE;
-        int ans = -1;
-                ans = j;
+}
